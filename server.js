97a04f78eb2258e72617087248a033d70118ca3c
Mark Thompson <markthompson@marks-mbp-2.lan>
2018-07-23 00:54:20 -0500

    Add code for episode 3

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // Static directory
-app.use(express.static('app/public'));
+app.use(express.static('public'));
 
 apiRoutes(app, db);
 
